202990aec12a54d4f18f213d2d23de904e3abd17
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-11-22 23:54:30 -0300

    Add files via upload

--- exploratory_data_analysis.R
+++ exploratory_data_analysis.R
@@ -11,3 +11,14 @@
 ### 1 - Gerar questões sobre os seus dados;
 ### 2 - Encontrar respostas visualizando, transformando e modelando os dados;
 ### 3 - Usar o que aprendeu para refinar questões/gerar novas questões.
+
+### Durante as fases iniciais da EDA, você deve se sentir à vontade para 
+### investigar cada ideia que lhe ocorrer. Algumas dessas ideias darão certo 
+### e outras serão becos sem saída.
+
+### A EDA é uma parte importante de qualquer análise de dados, mesmo que 
+### as principais questões de pesquisa sejam entregues a você de bandeja,
+### porque você sempre precisa investigar a qualidade dos seus dados.
+
+### Para fazer a limpeza de dados, você precisará implantar todas as 
+### ferramentas de EDA: visualização, transformação e modelagem.
