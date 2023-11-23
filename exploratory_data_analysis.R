876d4e7d49d4b726bf7dfe19a5353f43ec5883cf
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-11-22 23:47:38 -0300

    Add files via upload

--- exploratory_data_analysis.R
+++ exploratory_data_analysis.R
@@ -4,4 +4,10 @@
 # Data: 22/11/23 ---------------------------------------------------------------------------------------------------------------------------
 # Fonte: https://r4ds.hadley.nz/eda#introduction -------------------------------------------------------------------------------------------
 
+# Introdução -------------------------------------------------------------------------------------------------------------------------------
 
+### A análise exploratória dos dados serve para:
+
+### 1 - Gerar questões sobre os seus dados;
+### 2 - Encontrar respostas visualizando, transformando e modelando os dados;
+### 3 - Usar o que aprendeu para refinar questões/gerar novas questões.
