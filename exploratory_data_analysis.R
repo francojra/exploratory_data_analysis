0d9c1ff66590ab4be7138c50295d5921ba9d8b19
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-11-29 21:24:41 -0300

    Add files via upload

--- exploratory_data_analysis.R
+++ exploratory_data_analysis.R
@@ -193,10 +193,9 @@ incomum
 ### esses diamantes têm mais de uma polegada de comprimento, mas não custam 
 ### centenas de milhares de dólares!
 
-### É uma boa prática repetir sua análise com e sem valores discrepantes. 
+### É uma boa prática repetir suas análises com e sem valores discrepantes. 
 ### Se eles tiverem um efeito mínimo nos resultados e você não conseguir 
 ### descobrir por que estão ali, é razoável omiti-los e seguir em frente.
-
 ### No entanto, se eles tiverem um efeito substancial nos seus resultados,
 ### você não deve abandoná-los sem justificativa. Você precisará descobrir 
 ### o que os causou (por exemplo, um erro de entrada de dados) e divulgar 
